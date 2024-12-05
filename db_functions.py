adf7d566ff72b1f0e75e8027f60223312d4b8c36
Manu <manoela.bavia.senai@gmail.com>
2024-12-05 12:34:50 -0300

    manu

---
+++ db_functions.py
@@ -0,0 +1,24 @@
+from os import replace
+import mysql.connector
+from config import *
+
+def conectar_db():
+    conexao = mysql.connector.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME
+    )
+    # Alterar a linha abaixo
+    cursor = conexao.cursor(dictionary=True)
+    return conexao, cursor
+
+
+def encerrar_db(cursor, conexao):
+    cursor.close()
+    conexao.close()
+
+
+def limpar_input(campo):
+    campolimpo = campo.replace(".","").replace("/","").replace("-","").replace(" ","").replace("(","").replace(")","").replace("R$","")
+    return campolimpo
